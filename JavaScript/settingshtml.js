31bf492d14fae599b417edc3e1423f0ed4b8fa87
Voulu-2k6 <tms6415@gmail.com>
2025-09-10 17:12:18 -0500

    Update settingshtml.js

--- JavaScript/settingshtml.js
+++ JavaScript/settingshtml.js
@@ -2,8 +2,6 @@
 
 //get known preferences
 let userSettings = localStorage.getItem('userPreferences');
-console.log('I am here');
-if(userSettings){console.log('Found user settings, setting preferences to ' + userSettings);}
 let preferences = userSettings ? JSON.parse(userSettings) : { 
     'Capitals' : 0, 'Numbers' : 0, 'Specials' : 0, 'Words' : 10,
     adapt: false, key: null, 'endless': false, 'mySpecials' : []
