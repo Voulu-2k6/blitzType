1311b264fd03e6a5b1764d96af61928770c2a0b1
Voulu-2k6 <tms6415@gmail.com>
2025-09-09 23:00:36 -0500

    Update statshtml.js

--- JavaScript/statshtml.js
+++ JavaScript/statshtml.js
@@ -66,9 +66,10 @@ function getRelativeAccuracy(code){
 export function getAdvancements(keys){
     //implement key frequency in words.txt for constants.js
     //implement miss reduction in exam.js
+    let localStats = JSON.parse(localStorage.getItem('localStats'));
     for(let key of keys){
         console.log(key);
-        let keyStat = pageStats.keyStats[reverseKeyMap[key.id]];
+        let keyStat = localStats.keyStats[reverseKeyMap[key.id]];
         console.log(keyStat);
         let diamondStandard = (keyStat.accuracy > 0.97 && keyStat.totalHits >= 1200);
         let goldStandard = (keyStat.accuracy > 0.95 && keyStat.totalHits >= 500);
